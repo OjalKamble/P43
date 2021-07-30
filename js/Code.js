509712d68cd4a8d906ba71abb21d44670c6686a7
Ojal <75155992+OjalKamble@users.noreply.github.com>
2021-07-30 15:30:26 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    // add code for displaying the names and the hints.
+    textSize(15);
+    fill("black")
+    text("Hint: Always changing, not constant !!", 100,70)
+    text("Hint: Performs a particular task !!", 850,300)
+    text("Hint: Stores all Information !!", 100,440)
+    
+    fill("white")
+    textSize(15);
+    stroke("white");
+    text("N U C T F I O N", 850,280)
+    text("*R E V B A I L A*", 100,50)
+    text("A T A D A S B A", 100,420)
+}
